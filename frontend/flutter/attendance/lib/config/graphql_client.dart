c3c5910dd0d6882b5736c42b8df3058a8224cb70
robinson1505 <robinsonanthony1505@gmail.com>
2023-05-20 12:15:15 +0300

    start with login of the student

--- frontend/flutter/attendance/lib/config/graphql_client.dart
+++ frontend/flutter/attendance/lib/config/graphql_client.dart
@@ -1,15 +1,22 @@
 // import "package:flutter/material.dart";
 import "package:flutter/material.dart";
 import "package:graphql_flutter/graphql_flutter.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 const String uri = "http://192.168.1.3:4000/attendance";
 
 final HttpLink httpLink = HttpLink(uri);
+final AuthLink authLink = AuthLink(getToken: () async => getTokenFromStorage());
+final Link link = authLink.concat(httpLink);
+
 ValueNotifier<GraphQLClient> client =
-    ValueNotifier(GraphQLClient(link: httpLink, cache: GraphQLCache()));
+    ValueNotifier(GraphQLClient(link: link, cache: GraphQLCache()));
 
-
-
+Future<String> getTokenFromStorage() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString('token') ?? '';
+  return token;
+}
 // final AuthLink authLink = AuthLink(
 //   getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
 // );
--- frontend/flutter/attendance/lib/main.dart
+++ frontend/flutter/attendance/lib/main.dart
@@ -1,61 +1,154 @@
 import 'package:attendance/config/graphql_client.dart';
-import 'package:attendance/screens/splash_screen.dart';
+// import 'package:attendance/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(const AttendanceApp());
+void main() => runApp(AttendanceApp());
 
 class AttendanceApp extends StatelessWidget {
-  const AttendanceApp({super.key});
+  AttendanceApp({super.key});
+
+  final TextEditingController regnumberController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    const String fetchTimetable = """
-query{
-  getAllTimetable{
-    id
-    timetable_code
-    module_day
-  }
-}
-""";
-
     return GraphQLProvider(
       client: client,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: " Attendance App",
+        title: "Attendance App",
         home: Scaffold(
-          body: Query(
-              options: QueryOptions(document: gql(fetchTimetable)),
-              builder: ((result, {fetchMore, refetch}) {
-                if (result.hasException) {
-                  return Center(
-                    child: Text(result.exception.toString()),
-                  );
-                }
-                if (result.isLoading) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                List timetables = result.data!['getAllTimetable'];
-                return ListView.builder(
-                    itemCount: timetables.length,
-                    itemBuilder: (context, index) {
-                      final timetable = timetables[index];
+          appBar: AppBar(
+            title: const Text(
+              "Mutation",
+            ),
+            centerTitle: true,
+          ),
+          body: Mutation(
+            options: MutationOptions(
+                document: gql('''
+             mutation Login(\$studentRegistrationNumber: String!, \$password: String!){
+loginStudent(student_registration_number: \$studentRegistrationNumber, password: \$password)
+             }
 
-                      return ExpansionTile(
-                        title: Text(
-                          timetable['id'],
-                          style: const TextStyle(fontWeight: FontWeight.w700),
-                        ),
-                      );
-                    });
-              })),
+'''),
+                onCompleted: (resultData) async {
+                  if (resultData != null) {
+                    final token = resultData['loginStudent']['token'];
+                    await saveTokenToStorage(token);
+                    print(token);
+
+                    //  final AuthLink authLink = AuthLink(getToken: () => getTokenFromStorage());
+                    //  final Link link = authLink.concat(httpLink);
+                  }
+                }),
+            builder: (RunMutation runMutation, QueryResult? result) {
+              return Column(
+                children: [
+                  TextField(
+                    controller: regnumberController,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: passwordController,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      final regNumber = regnumberController.text;
+                      final password = passwordController.text;
+                      runMutation({
+                        'studentRegistrationNumber': regNumber,
+                        'password': password,
+                      });
+                    },
+                    child: const Text(
+                      "Submit",
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
   }
 }
+
+Future<void> saveTokenToStorage(String token) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('token', token);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// class AttendanceApp extends StatelessWidget {
+//   const AttendanceApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     const String fetchTimetable = """
+// query{
+//   getAllTimetable{
+//     id
+//     timetable_code
+//     module_day
+//   }
+// }
+// """;
+
+//     return GraphQLProvider(
+//       client: client,
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: " Attendance App",
+//         home: Scaffold(
+//           body: Query(
+//               options: QueryOptions(document: gql(fetchTimetable)),
+//               builder: ((result, {fetchMore, refetch}) {
+//                 if (result.hasException) {
+//                   return Center(
+//                     child: Text(result.exception.toString()),
+//                   );
+//                 }
+//                 if (result.isLoading) {
+//                   return const Center(
+//                     child: CircularProgressIndicator(),
+//                   );
+//                 }
+//                 List timetables = result.data!['getAllTimetable'];
+//                 return ListView.builder(
+//                     itemCount: timetables.length,
+//                     itemBuilder: (context, index) {
+//                       final timetable = timetables[index];
+
+//                       return ExpansionTile(
+//                         title: Text(
+//                           timetable['id'],
+//                           style: const TextStyle(fontWeight: FontWeight.w700),
+//                         ),
+//                       );
+//                     });
+//               })),
+//         ),
+//       ),
+//     );
+//   }
+// }
 //  home: SplashScreen(),
