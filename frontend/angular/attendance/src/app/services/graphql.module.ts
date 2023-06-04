1659751f26a0f5339abf86350c18f7362410173b
robinson1505 <robinsonanthony1505@gmail.com>
2023-06-04 20:15:19 +0300

    parse the token in front end

--- frontend/angular/attendance/src/app/services/graphql.module.ts
+++ frontend/angular/attendance/src/app/services/graphql.module.ts
@@ -15,7 +15,7 @@ const uri = 'http://localhost:4000/attendance'; // <-- add the URL of the GraphQ
     const token = localStorage.getItem('token');
     if (token) {
       operation.setContext({
-        headers: new HttpHeaders().set('Authorization', token),
+        headers: new HttpHeaders().set('Authorization', JSON.parse(token)),
       });
     }
     return forward(operation);
